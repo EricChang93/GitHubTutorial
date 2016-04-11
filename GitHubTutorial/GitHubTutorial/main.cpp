dabffddf15a76e668f7b956f105c631fc97584b3
Eric Chang <changeric0903@gmail.com>
2016-04-11 10:32:45 -0700

    new C++ file

---
+++ GitHubTutorial/GitHubTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitHubTutorial
+//
+//  Created by Eric Chang on 4/11/16.
+//  Copyright © 2016 Eric. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
